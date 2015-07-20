62662dafe0011013e4fbd3f6e633516528047be5
Sebastian Palacio <Sebastian.Palacio@dfki.de>
2015-07-20 12:01:49 +0200

    Revised grid area computation and interface for retrieving the neighborhood

--- dbclasd.py
+++ dbclasd.py
@@ -13,34 +13,52 @@ import traceback
 import numpy as np
 
 from sklearn.neighbors import NearestNeighbors
+from matplotlib import pyplot as plt
 
-
-def cluster_area(pts):
+def cluster_area(pts, nnfinder):
     """
     Approximate the area of a set of candidate points belonging to a cluster.
     :param pts:
-    :return:
+    :return: area, grid_length
     """
-    clust_nnfinder = NearestNeighbors(2, algorithm='ball_tree', p=1).fit(pts)
-    grid_length = max(clust_nnfinder.kneighbors(pts)[0][:, 1])  # Max NN distance of pts
+    nnfinder = NearestNeighbors(2, algorithm='ball_tree', p=2).fit(pts)
+    grid_length = max(nnfinder.kneighbors(pts)[0][:, 1])  # Max NN distance of pts
     # TODO: adjust the offset to fit in one cell the points with the largest distance (=grid_length)
-    grid_x_lims = np.arange(np.ceil((pts[:, 0].max() - pts[:, 0].min()) / grid_length)) * grid_length + pts[:, 0].min()
-    grid_y_lims = np.arange(np.ceil((pts[:, 1].max() - pts[:, 1].min()) / grid_length)) * grid_length + pts[:, 1].min()
+    grid_x_lims = np.arange(np.ceil((pts[:, 0].max() - pts[:, 0].min()) / grid_length) + 1) * grid_length + pts[:, 0].min()
+    grid_y_lims = np.arange(np.ceil((pts[:, 1].max() - pts[:, 1].min()) / grid_length) + 1) * grid_length + pts[:, 1].min()
     grid = np.histogram2d(pts[:, 0], pts[:, 1], bins=[grid_x_lims, grid_y_lims])[0]
-    return (grid >= 1).sum() * grid_length
+
+    # # Plot grid (Take it out afterwards)
+    # plt.scatter(pts[:, 0], pts[:, 1], c='b')
+    # for xbin in grid_x_lims:
+    #     plt.plot([xbin, xbin], [grid_y_lims[0], grid_y_lims[-1]], 'g--')
+    # for ybin in grid_y_lims:
+    #     plt.plot([grid_x_lims[0], grid_x_lims[-1]], [ybin, ybin], 'r--')
+
+    return (grid >= 1).sum() * grid_length, grid_length
 
 
 
-def retrieve_neighborhood(cluster, dists, pt_idx):
+def retrieve_neighborhood(cluster, nnfinder):
     """
     Part of the dbclasd algorithm
     :param cluster: cluster
-    :param dists: distances from all points
+    :param nnfinder: distances from all points
     :param pt_idx: point
     :return:list of points
     """
     # Calculate radius r > sqrt(A/PI * (1 - 1/N^(1/N)))
-    area = cluster_area(cluster[pt_idx])
+    area, grid_length = cluster_area(cluster, nnfinder)
+    N = float(len(cluster))
+    r = np.sqrt((area/np.pi) * (1 - 1/N**(1/N)))
+    # The paper doesn't define how large should the radius be (it just defines a lower bound). It makes sense to keep
+    # it below the grid length, otherwise, an epsilon is added to comply with the strict lower bound definition
+    if r > grid_length:
+        r += (grid_length - r) / 2.0
+    else:
+        r += 0.0001
+    query_nn_dists, query_nn_idxs = nnfinder.kneighbors([cluster[0]])
+    return query_nn_idxs[query_nn_dists <= r]
 
 
 def expand_cluster(cluster):
@@ -73,12 +91,14 @@ def dbclasd(pts):
     cands = []  # Candidates
     unsuccessful_cands = []
     proccessed_pts = []
-    nnfinder = NearestNeighbors(30, algorithm='ball_tree', p=1).fit(pts)
+    nnfinder = NearestNeighbors(30, algorithm='ball_tree', p=2).fit(pts)
     for pt_idx, pt in enumerate(pts):
         if pt_idx not in assigned_cands:
-            new_clust, new_clust_dists = nnfinder.kneighbors([pt])  # It includes pt_idx already. shape = (1, size)
-            for clust_pt_idx in new_clust:
-                answers = retrieve_neighborhood(new_clust, new_clust_dists, clust_pt_idx)
+            new_clust_dists, new_clust_idxs = nnfinder.kneighbors([pt])  # It includes pt_idx already. shape = (1, size)
+            new_clust_idxs = new_clust_idxs.flatten()
+            new_clust_dists = new_clust_dists.flatten()
+            for clust_pt_idx in new_clust_idxs:
+                answers = retrieve_neighborhood(pts[new_clust_idxs], nnfinder)
 
 
 
