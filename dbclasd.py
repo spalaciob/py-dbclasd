8fe20efe0a457774ce27f8530c725a386ead27d1
Sebastian Palacio <Sebastian.Palacio@dfki.de>
2015-07-24 10:05:00 +0200

    Put some more documentation into the source.

--- dbclasd.py
+++ dbclasd.py
@@ -25,7 +25,7 @@ def color_marker_generator(n, cm='jet'):
     :param cm: color map (from matplotlib.cm) to be used as color range.
     :return: a color and a shape. It first iterates over the colors, then the shapes.
     """
-    colors = plt.cm.get_cmap('jet')
+    colors = plt.cm.get_cmap(cm)
     shapes = ',.ov^v<>12348sp*hH+xdD|_'
     n = min(n, colors.N * len(shapes))
     mark_iter = np.round(np.linspace(0, colors.N * len(shapes), n)).astype('int')
@@ -35,9 +35,10 @@ def color_marker_generator(n, cm='jet'):
 
 def cluster_area(pts, nnfinder):
     """
-    Approximate the area of a set of candidate points belonging to a cluster.
-    :param pts:
-    :return: area, grid_length
+    Approximate the area of a set of candidate points belonging to a cluster. This is only necessary to compute the
+    lower bound for the radius of "retrieve_heighborhood".
+    :param pts: NxM 2d-array with N points of M dimensions.
+    :return: area, grid_length (both floating point values)
     """
     nnfinder = NearestNeighbors(2, algorithm='ball_tree', p=2).fit(pts)
     grid_length = max(nnfinder.kneighbors(pts))
@@ -56,56 +57,63 @@ def cluster_area(pts, nnfinder):
     return (grid >= 1).sum() * grid_length, grid_length
 
 
-def retrieve_neighborhood_area(allpts, cluster_idxs, nnfinder):
+def retrieve_neighborhood_area(allpts, cluster_idxs, nnfinder, pt_idx=None):
     """
-    Part of the dbclasd algorithm
-    :param cluster: cluster
-    :param nnfinder: distances from all points
-    :param pt_idx: point
-    :return:list of points
+    Retrieve candidates to expand a given cluster. The radius is set to the largest 1-NN distance within all cluster
+    points. The area is only computed to guarantee that the condition has been met.
+    :param allpts: NxM 2d-array with N points of M dimensions. These are all points in the dataset.
+    :param cluster_idxs: the indices whthin allpts of the (partial) cluster to be evaluated/expanded.
+    :param nnfinder: instance of scipy's NearestNeighbor fit with all points in the dataset.
+    :param pt_idx: index within allpts of the reference point to be checked for neighboring candidates to expand
+    the cluster. If None, the first index of cluster_idxs is going to be used instead.
+    :return: list of point indices that are within a radius r from the query point, including the query point.
     """
     cluster = allpts[cluster_idxs]
+
     # Calculate radius r > sqrt(A/PI * (1 - 1/N^(1/N)))
     area, grid_length = cluster_area(cluster, nnfinder)
     N = float(len(cluster))
     lower_bound = np.sqrt((area/np.pi) * (1 - 1/N**(1/N)))
     r = grid_length
+
     try:
         assert r > lower_bound, 'Radius does not meet the lower bound criteria.'
     except AssertionError:
         r = lower_bound * 1.01
-    # The paper doesn't define how large should the radius be (it just defines a lower bound). It makes sense to keep
-    # it below the grid length, otherwise, an epsilon is added to comply with the strict lower bound definition
-    if r > grid_length:
-        r += (grid_length - r) / 2.0
-    else:
-        r += 0.0001
-    query_nn_dists, query_nn_idxs = nnfinder.kneighbors([cluster[0]])
+
+    if pt_idx is None:
+        pt_idx = cluster_idxs[0]
+
+    query_nn_dists, query_nn_idxs = nnfinder.kneighbors([allpts[pt_idx]])
     return query_nn_idxs[query_nn_dists <= r]
 
 
-def retrieve_neighborhood_simple(allpts, cluster_idxs, nnfinder, pt_idx):
+def retrieve_neighborhood_simple(allpts, cluster_idxs, nnfinder, pt_idx=None):
     """
-    Return neighboring candidates to the first point in cluster, which are within a given radius r. r is the defined
-    as the largest distance between all possible nearest neighbors within cluster.
-    :param cluster:
-    :param nnfinder:
-    :param pt_idx:
-    :return: None
+    Retrieve candidates to expand a given cluster. The radius is set to the largest 1-NN distance within all cluster
+    points. This method omits the lower bound check and follows directly the definition for the radius.
+    :param allpts: NxM 2d-array with N points of M dimensions. These are all points in the dataset.
+    :param cluster_idxs: the indices whthin allpts of the (partial) cluster to be evaluated/expanded.
+    :param nnfinder: instance of scipy's NearestNeighbor fit with all points in the dataset.
+    :param pt_idx: index within allpts of the reference point to be checked for neighboring candidates to expand
+    the cluster. If None, the first index of cluster_idxs is going to be used instead.
+    :return: list of point indices that are within a radius r from the query point, including the query point.
     """
     cluster = allpts[cluster_idxs]
     two_nnfinder = NearestNeighbors(2, algorithm='ball_tree', p=2).fit(cluster)
     r = two_nnfinder.kneighborgs(cluster)[0][:, 1].max()  # Max NN distance of pts
+    if pt_idx is None:
+        pt_idx = cluster_idxs[0]
     query_nn_dists, query_nn_idxs = nnfinder.kneighbors([allpts[pt_idx]])
-    return query_nn_idxs[query_nn_dists[1:] <= r]  # Discard the input point itself
+    return query_nn_idxs[query_nn_dists <= r]  # Discard the input point itself
 
 
 def dbclasd(pts):
     """
-    Implementation of the DBCLASD clustering algorithm
+    Implementation of the DBCLASD clustering algorithm.
     :param pts: input points as 2D-array where the first axis is the number of points and the second, the dimensions
     of each point
-    :return: clusters (not sure yet)
+    :return: 1d-array with labels for each point in pts.
     """
     assigned_cands = np.zeros(len(pts)) - 1
     candidates = []  # Candidates
@@ -140,11 +148,14 @@ def dbclasd(pts):
                     new_candidate = candidates.pop(0)
                     chisq_0, p_0 = sci_chisquare(new_clust_dists)
                     new_clust_idxs = np.r_[new_clust_idxs, new_candidate]
-                    new_clust_dists = two_nnfinder.kneighbors(pts[new_clust_idxs])[0][:, 1]
+                    new_clust_dists = np.r_[new_clust_dists, two_nnfinder.kneighbors(pts[new_candidate])[0][:, 1]]
                     chisq_1, p_1 = sci_chisquare(new_clust_dists)
-                    print int(p_0*10000), int(p_1*10000)
-                    if int(p_0*10000)-1 <= int(p_1*10000) <= int(p_0*10000)+1:
+                    # A chi-square test us usually controlled through the alpha or p-value. Here we can say we accept
+                    # the null-hypothesis if the p-value doesn't grow w.r.t. the previous one. The problem with this
+                    # test is that it doesn't change too much by adding just one point to the sample.
+                    if np.round(p_0, 5) <= np.round(p_1, 5):
                         # Retrieve and update answers once more
+                        answer_idxs = []
                         for clust_pt_idx in new_clust_idxs:
                             query_nn_dists, query_nn_idxs = nnfinder.kneighbors([pts[clust_pt_idx]])
                             answer_idxs = query_nn_idxs[query_nn_dists[1:] <= r]  # Discard the input point itself
@@ -155,7 +166,7 @@ def dbclasd(pts):
                                 proccessed_pts.append(c_idx)
                         change = True
                     else:
-                        print 'distribution change!'
+                        print 'Distribution change: %.5f > %.5f' % (p_0, p_1)
                         unsuccessful_cands.append(new_candidate)
                 candidates = unsuccessful_cands[:]
                 unsuccessful_cands = []
@@ -164,6 +175,18 @@ def dbclasd(pts):
 
 
 def load_data(fpath):
+    """
+    Loads a dataset from a text file. It expects the data to be 2-dimensional and have a format like so:
+
+    12345   98765\n
+    56789   01234\n
+    ...
+
+    There can be an arbitrary number of spaces between the two numbers in a line and a new line at the end of each line.
+    In case the dataset is too big (> 10000 points), a random sample of 10000 points is returned instead.
+    :param fpath: Path in the file system where the text file lies.
+    :return: a 2d array with at most 10000 points loaded.
+    """
     dim = len(open(fpath).readline().split())
     syn_data = np.array(open(fpath).read().split(), dtype='float')
     syn_data = syn_data.reshape(syn_data.size/dim, dim)
@@ -176,7 +199,7 @@ def load_data(fpath):
 
 
 def main(opts):
-    """Main loop"""
+    """Main loop loads the data, performs the clustering on that data and plots it (assuming is 2D)"""
     pts = load_data(opts.infile)
     labels = dbclasd(pts)
     colors = color_marker_generator(np.unique(labels).size)
